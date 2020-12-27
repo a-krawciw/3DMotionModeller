f478d480c956de4127464f65c26609e4f08babd8
a-krawciw <freakyfactoid@gmail.com>
2020-12-27 10:44:38 -0800

    Moon sim

--- main.py
+++ main.py
@@ -8,19 +8,18 @@ from dynamics3d import g
 
 matplotlib.use('Qt5Agg')
 
-moon = Moon(1020, step_time=200)
-moon.run_for_time(2500000)
+moon = Moon(1104, step_time=400)
+moon.run_for_time(20000000)
 
 f = plt.figure()
-ax = f.add_subplot(111, projection='3d')
+ax = f.add_subplot(111)
 
 moon_pos = moon.position_history
-ax.plot(moon_pos.x, moon_pos.y, moon_pos.z, label="Numerical Sim")
+ax.plot(moon_pos.x, moon_pos.y, label="Numerical Sim")
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-
+ax.grid()
 f2 = plt.figure()
 ax2 = f2.add_subplot(111)
-ax2.plot(moon.time, moon.velocity_history.magnitude)
+ax2.plot(moon.time, moon_pos.magnitude)
 plt.show()
